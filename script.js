c356f8ab22df95b3d2e0713eae4a94791d998cca
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-06-28 12:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function convertToRoman(num) {
     };
 
   //your code here
+	 let result = '';
+
+    for (const { value, numeral } of romanMap) {
+        while (num >= value) {
+            result += numeral;
+            num -= value;
+        }
+    }
+
+    return result;
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
